52b1d7210c6f891bb33b9276c7d02a60e00b40ad
elim1988 <jlim062@aucklanduni.ac.nz>
2014-02-10 23:56:57 +1300

    some functions

---
+++ load_dir.R
@@ -0,0 +1,11 @@
+# load.dir is a function that converts annoying backslashes present in directories (for windows)
+# into slashes.
+# I wrote this just to use it in the other function, "source.edit". Otherwise, it doesn't have much use.
+
+load.dir =
+  function ()
+    {
+      Dir.1 <- file.choose()
+      if (grepl("[\\\\]", Dir.1) == TRUE) gsub("\\\\","/", Dir.1)
+        else print(Dir.1)
+    }
---
+++ source_edit.R
@@ -0,0 +1,39 @@
+# source.edit is a function that replaces unwanted texts/typos in source document with desired texts,
+# and saves the output.
+#
+# Arguments:
+#   
+#   old.dir = source document directory 
+#     (if left empty, file.choose() window opens to select the source document file)
+#   
+#   new.dir = directory with desired file name for the output 
+#     (if left empty, output is saved into the directory of the source document with "New" added to the file name)
+#
+#   old.txt = text that requires editting
+#
+#   new.txt = text that is used as an edit
+
+source.edit = 
+  function (old.dir = NULL, new.dir = NULL, old.txt, new.txt)
+    {
+      if (is.null(old.dir) == TRUE)
+        {
+          source.dir <- load.dir()
+          sourceDoc <- readLines(source.dir)
+        }
+          else sourceDoc <- readLines(old.dir)
+    
+      if (is.null(new.dir) == TRUE)
+        {
+          output.dir <- source.dir
+          output.file <- gsub('^(.+[/])','\\1New \\2',output.dir)
+        }
+        else
+          output.file = new.dir
+          new.sourceDoc <- gsub(old.txt, new.txt, sourceDoc)
+          writeLines(new.sourceDoc, output.file)
+    }
+
+
+# example:
+source.edit(old.txt = "Argument", new.txt = "(HERE)")
