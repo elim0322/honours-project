f98d7385301cd5f00fe4f6f9d1356177f9be6ba7
elim1988 <jlim062@aucklanduni.ac.nz>
2014-02-20 20:18:40 +1300

    processRinline update

--- sew.R
+++ sew.R
@@ -1,9 +1,20 @@
-
 copyInline <- function(line) {
-    temp <- gsub("^.*<!--rinline", "<!--keep.rinline", line)
+    temp <- gsub("<!--\\s*rinline", "<!--keep.rinline", line)
     line <- gsub("-->.*$", paste("-->", temp), line)
     line
 }
+
+processRinline <- function(line) {
+    mark <- gsub("(<!--rinline(.+?)-->)", "~MARKER~\\1~MARKER~", line)
+    temp <- unlist(strsplit(mark, "~MARKER~"))
+    lines <- grep("<!--rinline", temp)
+    for (i in 1:length(lines)) {
+      temp[lines][i] <- copyInline(temp[lines][i])
+    }
+    result <- paste(temp, collapse = " ")
+    result
+}
+
 
 sew <- function(infile = NULL, outfile = NULL) {
     if (is.null(infile)) {
@@ -12,31 +23,32 @@ sew <- function(infile = NULL, outfile = NULL) {
     if (!grepl("Rhtml$", infile))
         stop("infile is not an Rhtml file")
     src <- readLines(infile)
-  
-    R.begin <- grep("<!--begin.rcode", src)
-    R.end <- c(grep("end.rcode-->", src), length(src))
     
     ######################### inline R code chunks  #########################
     in.line <- grep("<!--rinline", src)
-  
+    
     for (i in 1:length(in.line)) {
-        src[in.line][i] <- copyInline(src[in.line][i])
+        src[in.line][i] <- processRinline(src[in.line][i])
     }
-    #########################################################################
-  
+    
+    ################# generate a list to keep R code chunks #################
+    R.begin <- grep("<!--begin.rcode", src)
+    R.end <- c(grep("end.rcode-->", src), length(src))
+    
     keep.list <- vector("list", (length(R.begin)+1))
-  
+    
     for(i in 1:length(R.begin)) {
         keep.list[[i]] <- src[R.begin[i]:R.end[i]]
         newFirstLine <- gsub("[.]r", ".keep", keep.list[[i]][1])
         keep.list[[i]][1] <- newFirstLine
     }
-
-    lines <- c(1, R.end[-length(R.end)] + 1)
-  
+    
+    ############################ write post.Rhtml ###########################
     if (is.null(outfile)) {
         outfile <- gsub("Rhtml","post.Rhtml", infile)
     }
+    
+    lines <- c(1, R.end[-length(R.end)] + 1)
     
     f <- file(outfile, open = "w")
     for (i in 1:length(R.end)) {
