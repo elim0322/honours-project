f45967cf48b20650e3069e1d0d9e8cb50b708ee8
elim1988 <jlim062@aucklanduni.ac.nz>
2014-03-01 13:04:15 +1300

    rip_old is the old rip()

--- rip.R
+++ rip.R
@@ -1,34 +1,51 @@
-library(XML)
-
 rip <- function(infile = NULL, outfile = NULL) {
     if (is.null(infile)) {
         infile <- load.dir()
-    } 
-    if (!grepl("html$", infile))
-        stop("infile is not an html file")
+    }
+    if (!grepl("post.html$", infile))
+        stop("infile is not a post.html file")
     
-    html <- htmlParse(infile)
+    src <- readLines(infile)
     
-    ################# Removing div class = "chunk" #################
-    chunks <- getNodeSet(html, "//div[@class = 'chunk']")
-    for (i in 1:length(chunks)) {
-        removeChildren(xmlParent(chunks[[i]]), kids = chunks[i])
+    # ".knitr.inline {" string seems to be always after 
+    # "<style type=\"text/css\">" string.
+    # Using this property, the correct "<style..." string is removed.
+    inline <- grep("[.]knitr[.]inline [{]", src)
+    style.begin <- grep('<style type=\"text/css\">', src)
+    style.end <- grep("</style>", src)
+    
+    if (any(style.begin == (inline-1))) {
+        start <- style.begin[which(style.begin == (inline-1))]
+        src <- src[-c(start:style.end[1])]
     }
     
-    ##################### Removing knitr style #####################
-    knitr <- getNodeSet(html, "//style[@type = 'text/css']")
-    for (i in 1:length(knitr)) {
-        style <- knitr[[i]]
-        # ONLY remove <style> if knitr put it there.
-        # Determine whether <style> was put there by knitr by
-        # looking for .knitr.inline rule within <style> content.
-        if (grepl(".knitr.inline", xmlValue(style))) {
-            removeChildren(xmlParent(style), kids = knitr[i])
-        }
+    ################ Remove "<div class=..." strings ################
+    chunks.begin <- grep('<div class=\"chunk\".+$', src)
+    chunks.end <- grep('</div></div>', src)
+    chunks <- vector("list", length(chunks.begin))
+    for (i in 1:length(chunks.begin)) {
+        chunks[[i]] <- chunks.begin[i]:chunks.end[i]
     }
+    lines <- unlist(chunks)
+    src <- src[-lines]
     
+    ###################### Remove inline codes ######################
+    inline <- grep('<code class=\"knitr inline\">', src)
+    src[inline] <- gsub('<code class=\"knitr inline"(.+?)</code>', 
+                        "", src[inline])
+    
+    ###################### Replace all "keep"s ######################
+    src <- gsub("<!--begin.keepcode", "<!--begin.rcode", src)
+    src <- gsub("<!--keep.rinline", "<!--rinline", src)
+    
+    ################### Remove extra spaces added ###################
+    # An empty space seems to be added before each R chunk
+    extra <- grep("<!--begin.rcode", src)-1
+    src <- src[-extra]
+    
+    ####################### Write return.Rhtml ######################
     if (is.null(outfile)) {
         outfile <- gsub("post.html","return.Rhtml", infile)
     }
-    saveXML(html, outfile)
+    write(src, outfile)
 }
---
+++ rip_old.R
@@ -0,0 +1,34 @@
+library(XML)
+
+rip.old <- function(infile = NULL, outfile = NULL) {
+    if (is.null(infile)) {
+        infile <- load.dir()
+    } 
+    if (!grepl("html$", infile))
+        stop("infile is not an html file")
+    
+    html <- htmlParse(infile)
+    
+    ################# Removing div class = "chunk" #################
+    chunks <- getNodeSet(html, "//div[@class = 'chunk']")
+    for (i in 1:length(chunks)) {
+        removeChildren(xmlParent(chunks[[i]]), kids = chunks[i])
+    }
+    
+    ##################### Removing knitr style #####################
+    knitr <- getNodeSet(html, "//style[@type = 'text/css']")
+    for (i in 1:length(knitr)) {
+        style <- knitr[[i]]
+        # ONLY remove <style> if knitr put it there.
+        # Determine whether <style> was put there by knitr by
+        # looking for .knitr.inline rule within <style> content.
+        if (grepl(".knitr.inline", xmlValue(style))) {
+            removeChildren(xmlParent(style), kids = knitr[i])
+        }
+    }
+    
+    if (is.null(outfile)) {
+        outfile <- gsub("post.html","return.Rhtml", infile)
+    }
+    saveXML(html, outfile)
+}
