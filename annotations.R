e7b852a37adc081d00fbfff1768174b48ec02106
elim1988 <jlim062@aucklanduni.ac.nz>
2014-03-26 20:51:03 +1300

    incomplete annotations

--- annotations.R
+++ annotations.R
@@ -13,15 +13,23 @@ where <- unlist(json$rows$ranges)
 where.start <- where[names(where) == "start"]
 where.end <- where[names(where) == "end"]
 
+############################ Create empty vectors and lists ###########################
 anno.tags <- vector("list", length = length(what))
 xpath.start <- vector("list", length = length(where.start))
 xpath.end <- vector("list", length = length(where.end))
 node.start <- vector("list", length = length(xpath.start))
 node.end <- vector("list", length = length(xpath.end))
 lineNumber <- vector("numeric", length = length(node.start))
-html <- htmlParse("Attempt.edit.html")
+
+# Create a copy of "Attempt.edit.html" and save it as "Attempt.save.html"
+#  so I can write lines in the for loop below.
+src <- readLines("Attempt.edit.html")
+f <- file("Attempt.save.html", open = "w")
+writeLines(src, f)
+close(f)
 
 for (i in 1:length(anno.tags)) {
+    # Create annotation tags.
     anno.tags[[i]] <- c('<p class="annotation">', 
                         paste("    The text", paste('"', what[i], '"', sep = ""),
                               "was annotated with the message", 
@@ -32,33 +40,30 @@ for (i in 1:length(anno.tags)) {
                                   "/ancestor::div[@class='chunk']", sep = "")
         xpath.end[[i]] <- paste("/html/body/div[@class='chunk']", where.end[i], 
                                 "/ancestor::div[@class='chunk']", sep = "")
+        html <- htmlParse("Attempt.save.html")
         node.start[[i]] <- getNodeSet(html, xpath.start[[i]])
         node.end[[i]] <- getNodeSet(html, xpath.end[[i]])
-        
+        lineNumber[i] <- getLineNumber(node.start[[i]][[1]])
+        src <- readLines("Attempt.save.html", warn = FALSE)
+        src <- append(src, anno.tags[[i]], after = lineNumber[i]-1)
+        f <- file("Attempt.save.html", open = "w")
+        writeLines(src, f)
+        close(f)
+    }
+}
+
         # Can't compare XML node sets directly so compare their lengths.
         # Not robust here.
         #  If the lengths of the node sets are equal, then use the first
         #  element of node.start
-        if (length(node.start[[i]]) == length(node.end[[i]])) {
-            lineNumber[i] <- getLineNumber(html, node.start[[i]][[1]])
-            else {
+        #if (length(node.start[[i]]) == length(node.end[[i]])) {
+        #    lineNumber[i] <- getLineNumber(html, node.start[[i]][[1]])
+        #    else {
                 # If their lengths are not the same, then use Which ever 
                 # has the least number of elements...
                 ################ Need Paul's help here ################
-                lineNumber[i] <- getLineNumber(html, )
+        #        lineNumber[i] <- getLineNumber(html, )
             }
         }
     }
 }
-
-
-
-
-
-
-
-
-# Just testing here.
-a <- getNodeSet(html, "/html/body/div[@class='chunk']/div[1]/div[1]/pre[1]/span[7]/ancestor::div[@class='chunk']") 
-getLineNumber("/html/body/div[@class='chunk']/div[1]/div[1]/pre[1]/span[7]/ancestor::div[@class='chunk']")
-getLineNumber(a[[2]])
