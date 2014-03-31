6d9cc5c2ecf9e5b894433e282a78b8a179071d56
elim1988 <eric@wifi-172-23-147-84.uoa-wifi.auckland.ac.nz>
2014-04-01 08:33:00 +1300

    annotations function

--- annotations.R
+++ annotations.R
@@ -1,45 +1,64 @@
 library(jsonlite)
 library(XML)
 
-########## Extract useful information from "test-annotations.txt" using json ##########
-temp6 <- readLines("test-annotations.txt", warn = FALSE)
-json <- fromJSON(temp6, simplifyVector = FALSE)
-
-numAnn <- length(json$rows)
-
+annotations <- function(infile = NULL, outfile = NULL, testfile = NULL) {
+    if (is.null(infile)) {
+        infile <- load.dir()
+    }
+    if (!grepl("edit.html$", infile))
+        stop("infile is not an edit.html file")
+    
+    ########## Extract useful information from "test-annotations.txt" using json ##########
+    temp <- readLines("test-annotations.txt", warn = FALSE)
+    json <- fromJSON(temp, simplifyVector = FALSE)
+    
+    # numAnn: number of annotations
+    #  (each ctrl+drag annotation is counted as a single annotation)
+    numAnn <- length(json$rows)
+    
     # Create a copy of "Attempt.edit.html" and save it as "Attempt.save.html"
     #  so I can write lines into "Attempt.save.html" and carry out other
     #  stuff in the for loop below:
-file.copy("Attempt.edit.html", "Attempt.save.html")
-
-for (i in 1:numAnn) {
-    ann <- json$rows[[i]]
-    who <- ann$user
-    what <- ann$quote
-    annotation <- ann$text
-    # ASSUME only one ranges value
-    where <- ann$ranges[[1]]  
-    where.start <- where$start
-    where.end <- where$end
-
-    # Create annotation tags.
-    anno.tags <- c('<p class="annotation">', 
-                   paste("    The text \"", what, 
-                         "\" was annotated with the message \"", 
-                         annotation, "\" by \"", who, "\"", sep=""),
-                   "</p>")
-    xpath.start <- paste("/html/body/div[@class='chunk']", where.start, 
-                         "/ancestor::div[@class='chunk']", sep = "")
-    xpath.end <- paste("/html/body/div[@class='chunk']", where.end, 
-                       "/ancestor::div[@class='chunk']", sep = "")
-    html <- htmlParse("Attempt.save.html")
-    node.start <- getNodeSet(html, xpath.start)
-    node.end <- getNodeSet(html, xpath.end)
-    # Improve next line to decide whether to use [[1]] or [[2]] or ...
-    lineNumber <- getLineNumber(node.start[[1]])
-    src <- readLines("Attempt.save.html", warn = FALSE)
-    src <- append(src, anno.tags, after = lineNumber[i]-1)
-    writeLines(src, "Attempt.save.html")
+    if (is.null(outfile)) {
+        outfile <- gsub("edit.html","save.html", infile)
+    }
+    file.copy(infile, outfile, overwrite = TRUE)
+    
+    for (i in 1:numAnn) {
+        ann <- json$rows[[i]]
+        who <- ann$user
+        what <- ann$quote
+        annotation <- ann$text
+        # ASSUME only one ranges value
+        where <- ann$ranges[[1]]
+        where.start <- where$start
+        where.end <- where$end
+    
+        # Create annotation tags.
+        anno.tags <- c('<p class="annotation" style = "background-color:coral">', 
+                       paste("    The text \"", what, 
+                             "\" was annotated with the message \"", 
+                             annotation, "\" by \"", who, "\"", sep=""),
+                       "</p>")
+        xpath <- paste("/html/body/div[@class='chunk']", where.start, 
+                             "/ancestor::div[@class='chunk']", sep = "")
+        html <- htmlParse("Attempt.save.html")
+        node <- getNodeSet(html, xpath)
+        
+        # xmlValues for the nodes.
+        txt <- lapply(node, xmlValue)
+        match <- function(t) {
+            # Substring "t" from the position of where$startOffset.
+            # Search for that pattern in what.
+            regexpr(what, substring(t, where$startOffset), fixed = TRUE)
+        }
+        # Which txt has the matching pattern.
+        temp <- sapply(txt, match)
+        # which(temp != -1) selects the correct node, as it will not be -1
+        # (being -1 indicates the pattern not matched in txt)
+        lineNumber <- getLineNumber(node[[which(temp != -1)]])
+        src <- readLines("Attempt.save.html", warn = FALSE)
+        src <- append(src, anno.tags, after = lineNumber - 1)
+        writeLines(src, "Attempt.save.html")
+    }
 }
-
- 
